cd7b16f249466af3ab7add5a48cc1353fb91e49d
Yolanda <ymoretto@gmail.com>
2020-01-18 10:50:02 -0300

    frontend

---
+++ categoria.php
@@ -0,0 +1,27 @@
+<?php require_once("header.php") ?>
+
+
+<section class="categoria container">
+
+<h1 class="h1categoria text-align-center">Categorias</h1>
+
+<nav class="row nav-categoria d-flex justify-content-center">
+
+
+<ul class="list-group col-lg-12">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+
+</nav>
+</section>
+
+
+
+
+
+
+<?php require_once("footer.php") ?>
---
+++ header.php
@@ -0,0 +1,35 @@
+<?php require_once("functions.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style-index.css">
+    <title>Blockbuster</title>
+</head>
+<body>
+
+<header class="menu-nav">
+    <nav class="navbar navbar-expand-lg color-navbar col-lg-12">
+        <!-- Image and text -->
+        <nav class="navbar">
+          <a class="navbar-brand logo-color" href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Blockbuster_logo.svg/1200px-Blockbuster_logo.svg.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Blockbuster
+          </a>
+        </nav>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link color-item" href="index.php">Home</a>
+          </li>
+          <li class="nav-item" >
+            <a class="nav-link color-item" href="categoria.php">Categorias</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</header>
