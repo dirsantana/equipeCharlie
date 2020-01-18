d99d6af791ce55b8adce7ecfb0e671c7c447e2a6
Yolanda <ymoretto@gmail.com>
2020-01-18 09:57:52 -0300

    home pronta

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style-index.css">
+    <title>Blockbuster</title>
+</head>
+<body>
+
+<header class="menu-nav">
+    <nav class="navbar navbar-expand-lg color-navbar col-lg-12">
+        <!-- Image and text -->
+        <nav class="navbar">
+          <a class="navbar-brand logo-color" href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Blockbuster_logo.svg/1200px-Blockbuster_logo.svg.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Blockbuster
+          </a>
+        </nav>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link color-item" href="#">Home</a>
+          </li>
+          <li class="nav-item" >
+            <a class="nav-link color-item" href="#">Categorias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link color-item" href="#">Melhores</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</header>
+
+
+<section class="categoria">
+
+<nav class="row nav-categoria d-flex justify-content-center">
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+</nav>
+
+<nav class="row nav-categoria d-flex justify-content-center">
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+</nav>
+
+<nav class="row nav-categoria d-flex justify-content-center">
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+    <div class="card col-lg-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card Category</h5>
+        <p class="card-text">Description of category.</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+</nav>
+</section>
+
+<section class="maisbaratos container">
+
+<h1 class=" h1home text-align-center"> Melhores Preços </h1>
+
+<nav class="row d-flex justify-content-center">
+
+
+
+<nav class="col-lg-12">
+    <ul class="list-group">
+      <li class="list-group-item">Piranhas Assassinas</li>
+      <li class="list-group-item">The Velocipastor</li>
+      <li class="list-group-item">Sharknado</li>
+      <li class="list-group-item">Sharkoctopus</li>
+    </ul>
+</nav>
+
+</nav>
+
+
+</section>
+
+
+
+
+
+<footer class="text-center">
+    <p>Blockbuster &copy; 2020</p>
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
